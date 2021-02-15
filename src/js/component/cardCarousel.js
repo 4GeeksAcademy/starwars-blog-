7bf6318166a2047d972e28681baa41e4d4944b2b
NicolasArayaB <nicolasaraya@gmail.com>
2021-02-15 04:09:51 +0000

    fetch people info

--- src/js/component/cardCarousel.js
+++ src/js/component/cardCarousel.js
@@ -1,20 +1,35 @@
 import { Carousel, Card, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-const CardCarousel = () => {
-	<Carousel>
-		<Carousel.Item>
-			<Card>
-				<Card.Img variant="top" src="" />
-				<Card.Body>
-					<Card.Title />
-					<Card.Text />
-					<Button variant="outline-primary">Learn More</Button>
-					<Button variant="outline-warning" className="likeBtn">
-						&#9825;
-					</Button>
-				</Card.Body>
-			</Card>
-		</Carousel.Item>
-	</Carousel>;
+const CardCarousel = props => {
+	let properties = props.properties;
+
+	return (
+		<Carousel>
+			<Carousel.Item>
+				<Card>
+					<Card.Img variant="top" src="" />
+					<Card.Body>
+						<Card.Title>
+							<span>{JSON.stringify(properties)}</span>
+						</Card.Title>
+						<Card.Text>
+							<p>Gender: </p>
+							<p>Hair Color: </p>
+							<p>Eye Color: </p>
+						</Card.Text>
+						<Button variant="outline-primary">Learn More</Button>
+						<Button variant="outline-warning" className="likeBtn">
+							&#9825;
+						</Button>
+					</Card.Body>
+				</Card>
+			</Carousel.Item>
+		</Carousel>
+	);
 };
+
+CardCarousel.propTypes = { properties: PropTypes.array };
+
 export default CardCarousel;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,27 +7,37 @@ export const Home = () => {
 	const [characters, setCharacters] = useState([]);
 	const [planets, setPlanets] = useState([]);
 	const [vehicles, setVehicles] = useState([]);
+	const [info, setInfo] = useState([]);
 
-	const getCategoryData = async category => {
-		const request = await fetch(`https://www.swapi.tech/api/${category}`, { method: "GET" });
-		const json = await request.json;
+	const getPeopleData = async () => {
+		const settings = {
+			method: "GET",
+			headers: { "Content-Type": "aplication/json" }
+		};
+
+		const request = await fetch(`https://www.swapi.tech/api/people`, settings);
+		const json = await request.json();
 		const data = json;
-		console.log(data);
-		switch (category) {
-			case "people":
-				setCharacters(data.results);
-				console.log(characters);
-				break;
-			case "planets":
-				setPlanets(data.results);
-				break;
-			case "starships":
-				setVehicles(data.result);
-		}
+
+		setCharacters(data);
+	};
+
+	const getInfo = async () => {
+		const settings = {
+			method: "GET",
+			headers: { "Content-Type": "aplication/json" }
+		};
+
+		const request = await fetch(`https://www.swapi.tech/api/people/1`, settings);
+		const json = await request.json();
+		const data = json;
+
+		setInfo(data.result);
 	};
 
 	useEffect(() => {
-		getCategoryData("people");
+		getPeopleData();
+		getInfo();
 	});
 
 	return (
@@ -35,12 +45,11 @@ export const Home = () => {
 			<Row>
 				<Col>
 					<h2 className="carouselHeading">Character</h2>
-					<span>{JSON.stringify(characters)}</span>
 				</Col>
 			</Row>
 			<Row>
 				<Col>
-					<CardCarousel />
+					<CardCarousel properties={info.properties} />
 				</Col>
 			</Row>
 		</Container>
